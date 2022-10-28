c9df62766898c9d0e25c5f7a7e1916093fff3a8b
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2022-10-28 16:56:03 +0000

    move script

--- upload/src/kaggle-upload.sh
+++ upload/src/kaggle-upload.sh
@@ -1,1 +1,24 @@
-../../shared/scripts/kaggle-upload.sh
+#!/usr/bin/env bash
+
+# move files into new assets dir
+mkdir assets
+cd assets
+ln -s ../hand/* .
+
+for filename in $(ls ../input); do
+    gunzip ../input/$filename -c > $(sed 's/.gz$//' <<< $filename)
+done 
+
+cd ..
+
+# check if dataset exists
+if kaggle d list --mine | grep "$(cat hand/dataset-metadata.json | jq -r ".title")" ; then
+    # if yes then version
+    kaggle d version -p assets --dir-mode skip -m "$(date)" ; 
+else 
+    # if no then create
+    kaggle d create -p assets --dir-mode skip
+fi
+
+# remove temp dir
+rm -rf assets
